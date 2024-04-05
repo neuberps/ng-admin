a7fab249d344c28bf7948347725dc422c2dba7d1
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-04-05 06:49:29 -0300

    Client Application Project Completed
    Crud at Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+
+import jQuery from 'jquery'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-admin';
+export class AppComponent implements AfterViewInit {
+  title = 'clientes-app';
+
+  ngAfterViewInit(){
+    (function($) {
+        "use strict";
+    
+        // Add active state to sidbar nav links
+        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+                if (this.href === path) {
+                    $(this).addClass("active");
+                }
+            });
+    
+        // Toggle the side navigation
+        $("#sidebarToggle").on("click", function(e) {
+            e.preventDefault();
+            $("body").toggleClass("sb-sidenav-toggled");
+        });
+    })(jQuery);
+  }
 }
---
+++ src/app/client/list/list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
+import { ClientService } from 'src/app/client.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  clients: Client[] = [];
+  selectedClient: Client;
+  successMessage: string;
+  errorMessage: string;
+
+  constructor(
+    private service: ClientService, 
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.service
+      .findAll()
+      .subscribe( response => this.clients = response);
+  }
+
+  createClient() {
+    this.router.navigate(['/form'])
+  }
+
+  confirmDeletion(client : Client) {
+    this.selectedClient = client;
+  }
+
+  deleteClient() {    
+    this.service.deleteId(this.selectedClient)
+      .subscribe( 
+          response => {
+            this.successMessage = 'Cliente excluído com sucesso!';
+            // Recarregar a página
+            window.location.reload();
+          },
+          error => {
+            console.error('Erro ao excluir cliente:', error);
+            this.errorMessage = 'Erro ao excluir cliente: ' + error.message;
+          }
+      );
+}
+}

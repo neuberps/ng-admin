28b4a42e94cf0031d9e03f86623c11da81baa8cc
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-04-09 10:02:26 -0300

    Update files

---
+++ src/app/client/service/searchfilter.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Client } from '../client';
+
+@Pipe({
+  name: 'searchfilter'
+})
+export class SearchfilterPipe implements PipeTransform {
+
+  transform(clients: Client[], searchValue:string): Client[] {
+
+    if(!clients || ! searchValue) {
+      return clients;
+    } else {
+      return clients.filter(client => client.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()));
+    }
+  }
+}

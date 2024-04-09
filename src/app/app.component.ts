584b72c1a46387b4c18f799663e253ceed17ebc0
Fanaina Alves <fanaiina.allves@gmail.com>
2024-04-09 11:32:37 -0300

    configurações dos AppComponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import jQuery from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'crud-service';
+
+  ngAfterViewInit(): void {
+    (function($){
+      "use strict";
+      var path = window.location.href;
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function(){
+        if (this.href === path){
+          $(this).addClass("active");
+        }
+      });
+
+      $("#sidebarToggle").on("click", function(e){
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
 }

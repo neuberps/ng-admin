90b685578b52e95d2e5c95a3b7e3efac5466981d
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-04-10 16:36:55 -0300

    Update routes.

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'user-form', component: UserFormComponent },
+  { path: 'user-form/:id', component: UserFormComponent },
+  { path: 'user-list', component: UserListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

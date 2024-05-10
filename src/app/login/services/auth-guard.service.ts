5a44f407bce835ee81b4b7650492d8da7a9fa44f
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-05-10 11:40:32 -0300

    Update routes guard-services.ts

--- src/app/login/services/auth-guard.service.ts
+++ src/app/login/services/auth-guard.service.ts
@@ -16,24 +16,18 @@ export class AuthGuardService implements CanActivate{
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const sessionToken = sessionStorage.getItem(LoginService.SESSION_TOKEN_KEY);
 
-    if (sessionToken) {
-      // Usuário logado
-      if (next.routeConfig?.path === 'login') {
-        // Redirecionar usuário logado para a página inicial
-        this.router.navigate(['/home']);
-        return false;
-      }
-      return true; // Permitir acesso a usuários logados
-    } else {
-      // Usuário não logado
-      if (next.routeConfig?.path === 'login') {
-        return true; // Permitir acesso à rota de login para usuários não logados
-      } else {
-        // Redirecionar usuários não logados para a página de login
-        this.router.navigate(['/login']);
-        return false;
-      }
+    if (sessionToken && next.routeConfig?.path === 'login') {
+      this.router.navigate(['/home']);
+      return false;
     }
+
+    if (!sessionToken && next.routeConfig?.path !== 'login') {
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+
   }
 
 }
